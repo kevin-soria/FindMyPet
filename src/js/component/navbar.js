aab69c856f4bc6c986c86165920458663f4f0e67
Kevin Soria <Kevin.soria@outlook.com>
2020-03-18 21:25:09 +0000

    landing1

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,11 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Lost & Furrends</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary">Log In! or Sign Up!</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,12 @@ import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Find My Pet!</h1>
 		<p>
-			<img src={rigoImage} />
+			<img src="https://images.ctfassets.net/cnu0m8re1exe/7sLmeD1tcL4UoIm0BjNaLh/22a9f42a4315361db96470f50b178e86/Dog-and-Cat.jpg?w=650&h=433&fit=fill" />
 		</p>
 		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
+			Lost a Pet Alert!
 		</a>
 	</div>
 );
