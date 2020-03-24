6494129319b1b8e0eb08d201c40e25f483f56ece
Kevin Soria <Kevin.soria@outlook.com>
2020-03-24 17:09:37 +0000

    adding pet editor and dash

--- src/js/views/peteditor.js
+++ src/js/views/peteditor.js
@@ -3,14 +3,28 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 
-export const Dashboard = props => {
+export const Peteditor = props => {
 	const { actions, store } = useContext(Context);
 	// const [phone, setPhone] = useState(store.contacts.phone);
-	const [username, setUsername] = useState(store.contacts.username);
-	const [first_name, setFirst_name] = useState(store.contacts.first_name);
-	const [last_name, setLast_name] = useState(store.contacts.last_name);
-	const [address, setAddress] = useState(store.contacts.address);
-	const [zipcode, setZipcode] = useState(store.contacts.zipcode);
+	const [petname, setUsername] = useState(store.contacts.petname);
+	const [animal, setAnimal] = useState(store.contacts.animal);
+	const [breed, setBreed] = useState(store.contacts.breed);
+	const [bodymarks, setBodymarks] = useState(store.contacts.bodymarks);
+	const [eyecolor, setEyecolor] = useState(store.contacts.eyecolor);
+	const [weight, setWeight] = useState(store.contacts.weight);
+	const [length, setLength] = useState(store.contacts.length);
+	const [height, setHeight] = useState(store.contacts.height);
+	const [furtype, setFurtype] = useState(store.contacts.furtype);
+	const [age, setAge] = useState(store.contacts.age);
+	const [gender, setGender] = useState(store.contacts.gender);
+	const [microchipno, setMicrochipno] = useState(store.contacts.microchipno);
+	const [diet, setDiet] = useState(store.contacts.diet);
+	const [activitylvl, setActivitylvl] = useState(store.contacts.activitylvl);
+	const [allergies, setAllergies] = useState(store.contacts.allergies);
+	const [medcond, setMedcond] = useState(store.contacts.medcond);
+	const [foodschedule, setFoodschedule] = useState(store.contacts.foodschedule);
+	const [sterilized, setSterilized] = useState(store.contacts.sterilized);
+	
 
 	return (
 		<div className="container">
@@ -56,7 +70,7 @@ export const Dashboard = props => {
 						/>
 					</div>
                     {/* -------------------------------------button to view/edit pets---------------------------- */}
-					<Link to={"/peteditor.js"}>
+					<Link to={"/pededitor.js"}>
 						<button
 							typse="button"
 							className="btn btn-primary form-control"
@@ -64,20 +78,20 @@ export const Dashboard = props => {
 							save Contact
 						</button>
 					</Link>
+                    {/* -------------------------------------homepage------------------------------- */}
                     <Link to={"/"}>
 						<button
 							typse="button"
 							className="btn btn-primary form-control"
 							onClick={() => actions.Dashboard(name, email, address, phone, props.match.params.meme)}>
-							save Contact
+						    or get back to Home Page
 						</button>
+					</Link>
+					<Link className="mt-3 w-100 text-center" to="/">
+						or get back to Home Page
 					</Link>
 				</form>
 			</div>
 		</div>
 	);
-};
-
-Dashboard.propTypes = {
-	match: PropTypes.object
 };
