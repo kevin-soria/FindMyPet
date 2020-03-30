b2cdeb8b732ee7640abbc377c9a4fbe1f729b5ec
KristinaFrants <cristina.chiticari@gmail.com>
2020-03-30 15:39:58 +0000

    new styles

--- src/js/component/bottomCards.js
+++ src/js/component/bottomCards.js
@@ -25,35 +25,37 @@ const Overlay = styled.div`
 	color: black;
 	animation: ${keyFrame} 2s ease;
 	animation-iteration-count: inherit;
-	opacity: ${props => props.opa};
+	opacity: ${props => props.opa && props.opop && props.op};
 `;
 export const BottomCards = () => {
 	const [opa, setOpa] = useState(0);
+	const [opop, setOpop] = useState(0);
+	const [op, setOp] = useState(0);
 	return (
 		<CardGroup>
-			<Card style={{ position: "relative" }} onMouseOver={() => setOpa(0.7)} onMouseLeave={() => setOpa(0)}>
+			<Card style={{ position: "relative" }} onMouseOver={() => setOpa(0.6)} onMouseLeave={() => setOpa(0)}>
 				<Card.Img
 					variant="top"
 					src="https://image.freepik.com/free-photo/lovely-pets-composition-with-sleepy-white-cat_23-2147997307.jpg"
 				/>
-				<Card.Footer />
+
 				<Overlay opa={opa}>Ciao</Overlay>
 			</Card>
-			<Card style={{ position: "relative" }} onMouseOver={() => setOpa(0.7)} onMouseLeave={() => setOpa(0)}>
+			<Card style={{ position: "relative" }} onMouseOver={() => setOpop(0.6)} onMouseLeave={() => setOpop(0)}>
 				<Card.Img
 					variant="top"
 					src="https://image.freepik.com/free-photo/cute-cat-with-sweater-hat_23-2148348114.jpg"
 				/>
-				<Card.Footer />
-				<Overlay opa={opa}>Ciao</Overlay>
+
+				<Overlay opa={opop}>Ciao</Overlay>
 			</Card>
-			<Card style={{ position: "relative" }} onMouseOver={() => setOpa(0.7)} onMouseLeave={() => setOpa(0)}>
+			<Card style={{ position: "relative" }} onMouseOver={() => setOp(0.6)} onMouseLeave={() => setOp(0)}>
 				<Card.Img
 					variant="top"
 					src="https://image.freepik.com/free-photo/ginger-tabby-kitten-stands-its-hind-legs_107760-273.jpg"
 				/>
-				<Card.Footer />
-				<Overlay opa={opa}>Ciao</Overlay>
+
+				<Overlay opa={op}>Ciao</Overlay>
 			</Card>
 		</CardGroup>
 	);
--- src/js/views/landingCards.js
+++ src/js/views/landingCards.js
@@ -2,47 +2,52 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 // import { url } from "inspector";
 
 let CardStyles = {
-	width: "25%",
+	width: "70%",
 	borderStyle: "solid",
-	marginLeft: "5%",
-	marginBottom: "2%",
+	marginLeft: "-10%",
+	marginBottom: "4%",
 	borderWidth: "2px"
 };
 
 let TextStyle = {
 	float: "right",
-	width: "40%",
-	textAlign: "right"
+	width: "100%"
 };
 
 export const LandingCards = () => {
 	return (
 		<Container>
-			<CardGroup>
-				<div style={CardStyles}>
-					<Card className="slideCard">
-						<Card.Img
-							id="cardSlide"
-							variant="top"
-							src="https://image.freepik.com/free-photo/scottish-fold-cat-blue-surface_23-2148181678.jpg"
-						/>
-					</Card>
-				</div>
-				<div className="wrapperImg">
-					<div id="slide" className="w-100">
-						<h3>Find Your Human</h3>
-						<p>
-							{" "}
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-							been the industrys standard dummy text ever since the 1500s, when an unknown printer took a
-							galley of type and scrambled it to make a type specimen book.
-						</p>
+			<Row>
+				<Col>
+					<div style={CardStyles}>
+						<Card className="slideCard">
+							<Card.Img
+								id="cardSlide"
+								variant="top"
+								src="https://image.freepik.com/free-photo/scottish-fold-cat-blue-surface_23-2148181678.jpg"
+							/>
+						</Card>
 					</div>
-				</div>
-			</CardGroup>
+				</Col>
+				<Col>
+					<div className="wrapperImg ">
+						<div id="slide" className="w-100">
+							<h3 className="headerStyle">Find Your Human</h3>
+							<p style={TextStyle}>
+								{" "}
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+								has been the industrys standard dummy text ever since the 1500s, when an unknown printer
+								took a galley of type and scrambled it to make a type specimen book.
+							</p>
+						</div>
+					</div>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
