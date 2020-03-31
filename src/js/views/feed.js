24efa35e3d94b26f6589c88d5bd849e34be20e29
Kevin Soria <Kevin.soria@outlook.com>
2020-03-31 18:57:03 +0000

    feed mapping but no info

--- src/js/views/feed.js
+++ src/js/views/feed.js
@@ -13,62 +13,71 @@ export const Feed = () => {
 	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
+	const { store, actions } = useContext(Context);
 
-	// const { store, actions } = useContext(Context);
 	return (
 		<div className="backgroundpic">
-			<Card className="feedbox" style={{ width: "18rem" }}>
-				<Card.Img
-					variant="top"
-					src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/missing-pet-lost-dog-cat-family-flyer-template-890095ffed1df13fbe001410e6ef2f2c_screen.jpg?ts=1561718514"
-				/>
-				<Card.Body>
-					<Card.Title>LOST DOG :(</Card.Title>
-					<Card.Text>Ive lost my dog, Please Help :(</Card.Text>
-					<Button onClick={handleShow} variant="primary">
-						Alert Me of a Lead
-					</Button>
-				</Card.Body>
-				<Modal show={show} onHide={handleClose}>
-					<Modal.Header closeButton>
-						<Modal.Title>Alert a Lead</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						<Form>
-							<Form.Group controlId="formBasicEmail">
-								<Form.Label>Email address</Form.Label>
-								<Form.Control type="email" placeholder="Enter email" />
-								<Form.Text className="text-muted">
-									Well never share your email with anyone else.
-								</Form.Text>
-							</Form.Group>
-							<Form.Group controlId="formBasicInfo">
-								<Form.Label>Pet Info</Form.Label>
-								<Form.Control type="info" placeholder="Enter info" />
-								<Form.Text className="text-muted" />
-							</Form.Group>
-							<Form.Group controlId="formBasicNumber">
-								<Form.Label>Phone Number</Form.Label>
-								<Form.Control type="phone" placeholder="Enter phone number" />
-								<Form.Text className="text-muted" />
-							</Form.Group>
-							<Form.Group controlId="formBasicMessage">
-								<Form.Label>Enter your message</Form.Label>
-								<Form.Control type="message" placeholder="Enter message" />
-								<Form.Text className="text-muted" />
-							</Form.Group>
-						</Form>
-					</Modal.Body>
-					<Modal.Footer>
-						<Button variant="secondary" onClick={handleClose}>
-							Close
-						</Button>
-						<Button variant="primary" onClick={handleClose}>
-							Send Alert
-						</Button>
-					</Modal.Footer>
-				</Modal>
-			</Card>
+			{store.alerts.map((alert, index) => {
+				return (
+					<li key={index} className="list-group-item">
+						<div className="row">
+							<Card className="feedbox" style={{ width: "18rem" }}>
+								<Card.Img
+									variant="top"
+									src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/missing-pet-lost-dog-cat-family-flyer-template-890095ffed1df13fbe001410e6ef2f2c_screen.jpg?ts=1561718514"
+								/>
+								<Card.Body>
+									<Card.Title>LOST DOG :(</Card.Title>
+									<Card.Text>Ive lost my dog, Please Help :(</Card.Text>
+									<Button onClick={handleShow} variant="primary">
+										Alert Me of a Lead
+									</Button>
+								</Card.Body>
+								<Modal show={show} onHide={handleClose}>
+									<Modal.Header closeButton>
+										<Modal.Title>Alert a Lead</Modal.Title>
+									</Modal.Header>
+									<Modal.Body>
+										<Form>
+											<Form.Group controlId="formBasicEmail">
+												<Form.Label>Email address</Form.Label>
+												<Form.Control type="email" placeholder="Enter email" />
+												<Form.Text className="text-muted">
+													Well never share your email with anyone else.
+												</Form.Text>
+											</Form.Group>
+											<Form.Group controlId="formBasicInfo">
+												<Form.Label>Pet Info</Form.Label>
+												<Form.Control type="info" placeholder="Enter info" />
+												<Form.Text className="text-muted" />
+											</Form.Group>
+											mpn{" "}
+											<Form.Group controlId="formBasicNumber">
+												<Form.Label>Phone Number</Form.Label>
+												<Form.Control type="phone" placeholder="Enter phone number" />
+												<Form.Text className="text-muted" />
+											</Form.Group>
+											<Form.Group controlId="formBasicMessage">
+												<Form.Label>Enter your message</Form.Label>
+												<Form.Control type="message" placeholder="Enter message" />
+												<Form.Text className="text-muted" />
+											</Form.Group>
+										</Form>
+									</Modal.Body>
+									<Modal.Footer>
+										<Button variant="secondary" onClick={handleClose}>
+											Close
+										</Button>
+										<Button variant="primary" onClick={handleClose}>
+											Send Alert
+										</Button>
+									</Modal.Footer>
+								</Modal>
+							</Card>
+						</div>
+					</li>
+				);
+			})}
 		</div>
 	);
 };
